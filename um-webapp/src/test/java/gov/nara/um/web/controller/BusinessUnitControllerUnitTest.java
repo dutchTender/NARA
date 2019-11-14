bffae0e8890e1840ae324c42573f5d1f9f1ada80
li zhang <lzhang421@gmail.com>
2019-11-14 00:40:32 -0500

    unit test skeleton for um api micro service.

--- um-webapp/src/test/java/gov/nara/um/web/controller/BusinessUnitControllerUnitTest.java
+++ um-webapp/src/test/java/gov/nara/um/web/controller/BusinessUnitControllerUnitTest.java
@@ -1,29 +1,45 @@
 package gov.nara.um.web.controller;
 
 import gov.nara.um.service.IBusinessUnitService;
-import gov.nara.um.service.impl.BusinessUnitServiceImpl;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { BusinessUnitController.class }, loader = AnnotationConfigContextLoader.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BusinessUnitController.class)
+@AutoConfigureMockMvc
 public class BusinessUnitControllerUnitTest {
 
-    @InjectMocks
-    private IBusinessUnitService service = new BusinessUnitServiceImpl();
+    @MockBean
+    private IBusinessUnitService service;
+
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
-    public final void checkingControllerExpectedInputOutput() {
+    public final void checkingControllerExpectedInputOutput() throws Exception {
         //
 
+        try {
+
+
+
+        }
+        catch (Exception ex){
+            System.out.println("exception occurred: "+ex);
+        }
 
     }
 }
